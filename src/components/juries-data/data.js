1cb27d71a39b2252bde4f7b9b49305be8dfcf857
Fadi <fady-naddaf@hotmail.com>
2022-12-28 22:42:43 +0100

    Creating Juries data

---
+++ src/components/juries-data/data.js
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    image: '../../../material/members-pictures/IMG-20221214-WA0001.jpg',
+    name: 'Asaad Taha',
+    profession: 'Journalist, producer & Documentary filmmaker',
+    description:
+      'Asaad is an Egyptian filmmaker, worked as a journalist for "Alhaya", "AlAhram", "AlSharq Alawsat" and more newspapers and weekly magazine. He also worked with TV channels such as "MBC", "AlJazeera" & "AlAraby TV". He Produced 25 documentary series, around 900 hours of documentaries which covered more than 80 countries around the world. He interviewed presidents, rebels and kings; like "Ali Ezzat Begovic" & "the Dalai Lama".',
+    awards: [
+      'Best Documentary Program at Cairo Film Festival, 1997.',
+      'Best Documentary at Kazan International Film Festival 2006 for making "El dorado".',
+      'Best Documentary at Kazan International Film Festival 2007 for the Film "Seven Tents".',
+      'Honored at the 2010 MIP Cannes Festival.',
+      'Honored at East Europe Documentary Film Festival in Asilah, Morocco in 2014.',
+    ],
+  },
+  {
+    image: '../../../material/members-pictures/IMG-20221214-WA0001.jpg',
+    name: 'Asaad Taha',
+    profession: 'Journalist, producer & Documentary filmmaker',
+    description:
+      'Asaad is an Egyptian filmmaker, worked as a journalist for "Alhaya", "AlAhram", "AlSharq Alawsat" and more newspapers and weekly magazine. He also worked with TV channels such as "MBC", "AlJazeera" & "AlAraby TV". He Produced 25 documentary series, around 900 hours of documentaries which covered more than 80 countries around the world. He interviewed presidents, rebels and kings; like "Ali Ezzat Begovic" & "the Dalai Lama".',
+    awards: [
+      'Best Documentary Program at Cairo Film Festival, 1997.',
+      'Best Documentary at Kazan International Film Festival 2006 for making "El dorado".',
+      'Best Documentary at Kazan International Film Festival 2007 for the Film "Seven Tents".',
+      'Honored at the 2010 MIP Cannes Festival.',
+      'Honored at East Europe Documentary Film Festival in Asilah, Morocco in 2014.',
+    ],
+  },
+];
