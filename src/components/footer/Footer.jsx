25e9a0e0abce6fb5a255cfbfc776d6460329c50c
Ali GÖK <97063504+aligoknl@users.noreply.github.com>
2023-01-01 16:29:21 +0100

    created footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,84 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.css";
+import { Facebook } from "@mui/icons-material";
+import { Instagram } from "@mui/icons-material";
+import { YouTube } from "@mui/icons-material";
+import PinDropIcon from "@mui/icons-material/PinDrop";
+import CallIcon from "@mui/icons-material/Call";
+import EmailIcon from "@mui/icons-material/Email";
+
+function Footer() {
+  //write code here
+
+  return (
+    <Box bottom={0} width="100%" bgcolor="first.main">
+      <div className="footer">
+        <div className="footer-logo-text-first">
+          <Link to="/">
+            <div className="footer-logo-text">
+              25 January <br></br>Film Festival
+            </div>
+          </Link>
+        </div>
+        <div className="footer-elements">
+          <div className="footer-left">
+            <div className="footer-follow-us"> Follow Us</div>
+            <div className="footer-social-logos">
+              <a
+                href="https://www.facebook.com/people/25-January-Film-Festival/100067079333985/?paipv=0&eav=AfayuibwgJ1ebVg5Cb468tLaMyO3sgbo5hKM7cOcWI6l_zUarkxZlkNW2O2EH-t7WEk&_rdr"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Facebook fontSize="medium" className="footer-social-logo" />
+              </a>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Instagram fontSize="medium" className="footer-social-logo" />
+              </a>
+              <a
+                href="https://www.youtube.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <YouTube fontSize="medium" className="footer-social-logo" />
+              </a>
+            </div>
+          </div>
+
+          <div className="footer-middle">
+            <Link to="/" className="footer-logo-text">
+              25 January <br></br>Film Festival
+            </Link>
+          </div>
+          <div className="footer-right">
+            <div className="footer-contact-us">Contact Us</div>
+            <div className="footer-contact-line">
+              <PinDropIcon fontSize="small" />
+              <div className="footer-contact">
+                Lorem Ipsum Street 00 1000AA Amsterdam
+              </div>
+            </div>
+            <div className="footer-contact-line">
+              <EmailIcon fontSize="small" />
+              <div className="footer-contact">loremipsum@gmail.com </div>
+            </div>
+            <div className="footer-contact-line">
+              <CallIcon fontSize="small" />
+              <div className="footer-contact">0612345678 </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        Copyright &copy; 2023 &bull; 25 January Film Festival
+      </div>
+    </Box>
+  );
+}
+
+export default Footer;
